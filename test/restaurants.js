6cb3afd93fc2dec3d2b4919bd10520522bb687ea
Preston Pham <prestonp08@gmail.com>
2013-05-06 18:01:06 +0400

    quick change

--- test/restaurants.js
+++ test/restaurants.js
@@ -4,11 +4,25 @@ var assert = require('assert')
 
 describe('Restaurant API', function() {
   describe('/restaurants', function() {
+
     before(function() {
     });
+
     it('should return list of restaurants', function(done) {
-        request.get('http://localhost:3001/restaurants.json', function(err, res, body) {
+      request.get('http://localhost:3001/restaurants', function(err, res, body) {
         assert.ok(body);
+        done();
+      });
+    });
+
+    it('should handle content negotiation', function(done) {
+      assert.ok(false);
+      done();
+    });
+  
+    it('should return restaurant by :id', function(done) {
+      request.get('http://localhost:3001/restaurants/43141431', function(err, res, body) {
+        assert(false);
         done();
       });
     });
